90920cad731eac49462523d3c1b2d066877cc0c2
hrymer <48784175+hrymer@users.noreply.github.com>
2019-04-09 23:30:19 +0300

    Add files via upload

--- PrintTimeIP.py
+++ PrintTimeIP.py
@@ -1,9 +1,30 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-print(s.getsockname()[0])
-s.close()
 
 
-import datetime
-print(datetime.datetime.now())
+def get_ip():
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = (s.getsockname()[0])
+    s.close()
+    return ip
+
+
+def get_time():
+    from time import gmtime, strftime
+    time = strftime("%H:%M:%S", gmtime())
+    return time
+
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'IP Address: ' + get_ip() + ' Time NOW: ' + get_time()
+
+if __name__ == '__main__':
+  app.run(host='127.0.0.1')
+
+
+
